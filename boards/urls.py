26c3a4a7127a210d467a9cf1aabcb7d51c93b7b9
Folafolu <ofolafolu@gmail.com>
2023-03-18 13:17:40 +0100

    Topics page view

--- boards/urls.py
+++ boards/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('board/<int:pk>', views.board_topics, name='board_topics')
 ]
