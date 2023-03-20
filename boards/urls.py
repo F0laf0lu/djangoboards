f7bfd08ea210a5ee51aa431cde0e1fdcc89d2c55
Folafolu <ofolafolu@gmail.com>
2023-03-20 21:28:47 +0100

    authentication, posts , replies

--- boards/urls.py
+++ boards/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('board/<int:pk>', views.board_topics, name='board_topics'),
-    path('board/<int:pk>/new_topic', views.new_topic, name='new_topic')
+    path('board/<int:pk>/new_topic', views.new_topic, name='new_topic'),
+    path('board/<int:pk>/<int:topic_pk>', views.topic_post, name='topic_post'),
+    path('board/<int:pk>/<int:topic_pk>/reply', views.reply_topic, name='reply_topic'),
 ]
--- chatboards/settings.py
+++ chatboards/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
-
+LOGIN_URL = 'login'
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
